1d0d85bcf61879f771c376a05a6f550d74e73759
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-27 03:28:35 +0000

    chore: bump manifest version to main

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 games { 'rdr3', 'gta5' } 
 author 'DirkScripts' 
 description 'Clean Animations | Pause menu for FiveM'
-version      '1.0.4' 
+version      '1.0.5' 
 
 shared_script {
   '@clean_lib/init.lua',
